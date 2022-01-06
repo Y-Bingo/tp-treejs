c3f6c62df81f983d2a1f08a6b150af93aadfec19
Bingo.Yang <y_bingo@163.com>
2022-01-07 00:28:39 +0800

    build: 取消 reload
    # Conflicts:
    #	webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -7,10 +7,12 @@ module.exports = {
 	devtool: 'inline-source-map',
 	cache: {
 		type: 'filesystem',
+		store: 'pack',
+		// compression: 'gzip',
 	},
 	entry: './src/index.ts',
 	output: {
-		filename: 'main.js',
+		filename: '[name].js',
 		path: path.resolve(__dirname, 'dist'),
 	},
 	resolve: {
@@ -37,12 +39,16 @@ module.exports = {
 		static: {
 			directory: path.join(__dirname, './'),
 		},
+		devMiddleware: {
+			// writeToDisk: true,
+		},
 		static: './',
 		compress: true, //如果为 true ，开启虚拟服务器时，为你的代码进行压缩。加快开发流程和优化的作用
 		host: 'localhost', // 设置主机名，默认为"localhost"
 		port: 9933, // 设置端口号,默认端口号为8080
 		historyApiFallback: true, //让所有404错误的页面定位到index.html
-		hot: true,
-		open: true, //启动服务器时，自动打开浏览器，默认为false
+		hot: 'only',
+		liveReload: false,
+		open: false, //启动服务器时，自动打开浏览器，默认为false
 	},
 };
