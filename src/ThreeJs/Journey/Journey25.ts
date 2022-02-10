cbf2f36875e25fe22fc62ea83576c7de32d4d031
Bingo.Yang <y_bingo@163.com>
2022-02-11 00:41:43 +0800

    feat: 添加环境贴图，使得更逼真

--- src/ThreeJs/Journey/Journey25.ts
+++ src/ThreeJs/Journey/Journey25.ts
@@ -21,7 +21,7 @@ export class Journey25 extends BaseJourney {
 	protected onCreating(): void {
 		// params
 		this.params = {
-			materialColor: 0xffeded,
+			envMapIntensity: 5,
 		};
 
 		// clock
@@ -40,30 +40,36 @@ export class Journey25 extends BaseJourney {
 		this.controls.enableDamping = true;
 
 		this.renderer.physicallyCorrectLights = true;
-		// shadow
-		// this.renderer.shadowMap.enabled = true;
-		// this.renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+		this.renderer.outputEncoding = THREE.sRGBEncoding;
+		this.renderer.toneMapping = THREE.ACESFilmicToneMapping;
+		this.renderer.shadowMap.enabled = true;
+		this.renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+
+		this.gui
+			.add(this.renderer, 'toneMapping', {
+				No: THREE.NoToneMapping,
+				Linear: THREE.LinearToneMapping,
+				Cineon: THREE.CineonToneMapping,
+				Reinhard: THREE.ReinhardToneMapping,
+				ACESF: THREE.ACESFilmicToneMapping,
+			})
+			.onFinishChange(() => {
+				this.renderer.toneMapping = Number(this.renderer.toneMapping);
+				this.updateAllMaterial();
+			});
+
+		this.gui.add(this.renderer, 'toneMappingExposure').min(0).max(10).step(0.01);
 	}
 
 	/**
 	 * @protected
 	 */
 	protected initLight(): void {
-		// const ambientLight = new THREE.AmbientLight(0xffffff, 0.7);
-		// this.scene.add(ambientLight);
-		// const directionalLight = new THREE.DirectionalLight(0xffffff, 0.2);
-		// directionalLight.castShadow = true;
-		// directionalLight.shadow.mapSize.set(1024, 1024);
-		// directionalLight.shadow.camera.far = 15;
-		// directionalLight.shadow.camera.left = -7;
-		// directionalLight.shadow.camera.top = 7;
-		// directionalLight.shadow.camera.right = 7;
-		// directionalLight.shadow.camera.bottom = -7;
-		// directionalLight.position.set(5, 5, 5);
-		// this.scene.add(directionalLight);
-
 		const directionalLight = new THREE.DirectionalLight(0xffffff, 3);
 		directionalLight.position.set(0.25, 3, -2.25);
+		directionalLight.shadow.mapSize.set(1024, 1024);
+		directionalLight.castShadow = true;
+		directionalLight.shadow.normalBias = 0.05;
 		this.scene.add(directionalLight);
 
 		this.gui.add(directionalLight, 'intensity').min(0).max(10).step(0.01).name('lightIntensity');
@@ -77,7 +83,7 @@ export class Journey25 extends BaseJourney {
 	 */
 	protected initCamera(): void {
 		const camera = new THREE.PerspectiveCamera(75, this.width / this.height, 0.1, 100);
-		camera.position.set(-5, 5, 0);
+		camera.position.set(-7, 7, 7);
 		this.camera = camera;
 		this.scene.add(camera);
 	}
@@ -88,6 +94,17 @@ export class Journey25 extends BaseJourney {
 	protected initModel(): void {
 		const scene = this.scene;
 		const cubeTextureLoader = new THREE.CubeTextureLoader();
+		const environmentMap = cubeTextureLoader.load([
+			'resource/journey/environmentMaps/3/px.jpg',
+			'resource/journey/environmentMaps/3/nx.jpg',
+			'resource/journey/environmentMaps/3/py.jpg',
+			'resource/journey/environmentMaps/3/ny.jpg',
+			'resource/journey/environmentMaps/3/pz.jpg',
+			'resource/journey/environmentMaps/3/nz.jpg',
+		]);
+		environmentMap.encoding = THREE.sRGBEncoding;
+		scene.background = environmentMap;
+		scene.environment = environmentMap;
 
 		const dracoLoader = new DRACOLoader();
 		dracoLoader.setDecoderPath('resource/draco/');
@@ -96,15 +113,19 @@ export class Journey25 extends BaseJourney {
 		gltfLoader.setDRACOLoader(dracoLoader);
 
 		gltfLoader.load(
-			'./resource/journey/models/FlightHelmet/glTF/FlightHelmet.gltf',
+			// './resource/journey/models/FlightHelmet/glTF/FlightHelmet.gltf',
+			'./resource/journey/models/hamburger/hamburger.glb',
 			gltf => {
 				const fightHelmet = gltf.scene;
-				fightHelmet.scale.set(10, 10, 10);
+				// fightHelmet.scale.set(10, 10, 10);
 				fightHelmet.position.set(0, -4, 0);
 				fightHelmet.rotation.y = -Math.PI * 0.5;
 				scene.add(fightHelmet);
 
 				this.gui.add(fightHelmet.rotation, 'y').min(-Math.PI).max(Math.PI).step(0.001).name('rotationY');
+				this.gui.add(this.params, 'envMapIntensity').min(0).max(10).step(0.01).onChange(this.updateAllMaterial.bind(this));
+
+				this.updateAllMaterial();
 			},
 			() => {
 				console.log('progress');
@@ -116,6 +137,17 @@ export class Journey25 extends BaseJourney {
 
 		const sphere = new THREE.Mesh(new THREE.SphereBufferGeometry(1, 32, 32), new THREE.MeshStandardMaterial());
 		scene.add(sphere);
+	}
+
+	private updateAllMaterial(): void {
+		this.scene.traverse(child => {
+			if (child instanceof THREE.Mesh && child.material instanceof THREE.MeshStandardMaterial) {
+				child.material.envMapIntensity = this.params.envMapIntensity;
+				child.material.needsUpdate = true;
+				child.receiveShadow = true;
+				child.castShadow = true;
+			}
+		});
 	}
 
 	/**
