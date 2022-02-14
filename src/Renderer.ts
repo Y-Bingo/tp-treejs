ce8f7c2cdd8e183f49d730378cc56de7e75ec1ed
Bingo.Yang <y_bingo@163.com>
2022-02-14 23:55:50 +0800

    feat: 调整 renderer 参数

--- src/Renderer.ts
+++ src/Renderer.ts
@@ -29,6 +29,13 @@ export class Renderer {
 
 	private setInstance(): void {
 		this.instance = new THREE.WebGLRenderer({ canvas: this.canvas, antialias: true });
+		this.instance.physicallyCorrectLights = true;
+		this.instance.shadowMap.type = THREE.PCFSoftShadowMap;
+		this.instance.outputEncoding = THREE.sRGBEncoding;
+		this.instance.toneMapping = THREE.CineonToneMapping;
+		this.instance.toneMappingExposure = 1.75;
+		this.instance.shadowMap.enabled = true;
+		this.instance.shadowMap.type = THREE.PCFSoftShadowMap;
 		this.instance.setClearColor(0x211d20, 1);
 		this.instance.setSize(this.sizes.width, this.sizes.height); // 设置渲染器的宽度和高度
 		this.instance.setPixelRatio(this.sizes.devicePixelRatio); // 设置设备像素比
