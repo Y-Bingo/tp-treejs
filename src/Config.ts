4c0b5908e3487c69f6eb04bae0e9a7335e2543c4
Bingo.Yang <y_bingo@163.com>
2022-03-20 20:26:08 +0800

    feat(demo-journey): post-processing

--- src/Config.ts
+++ src/Config.ts
@@ -169,4 +169,9 @@ export const APP_CONFIG = [
 		type: EAppType.JOURNEY,
 		title: 'Journey - modified material',
 	},
+	{
+		id: '32',
+		type: EAppType.JOURNEY,
+		title: 'Journey - post-processing',
+	},
 ];
